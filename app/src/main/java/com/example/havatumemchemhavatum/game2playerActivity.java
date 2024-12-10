f4e7648fc7faa07a30fbc239d242059d6c2cc21a
gven <abelyangevorg935@gmail.com>
2024-12-10 19:05:05 +0400

    A

--- app/src/main/java/com/example/havatumemchemhavatum/game2playerActivity.java
+++ app/src/main/java/com/example/havatumemchemhavatum/game2playerActivity.java
@@ -22,6 +22,7 @@ public class game2playerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2player);
+
         Button show = findViewById(R.id.show);
         Button step_change = findViewById(R.id.step_change);
         Button deal = findViewById(R.id.deal);
@@ -45,22 +46,24 @@ public class game2playerActivity extends AppCompatActivity {
         TextView player2card9 = findViewById(R.id.card_bottom_9);
         RelativeLayout player1cards = findViewById(R.id.top_cards);
         RelativeLayout player2cards = findViewById(R.id.bottom_cards);
+
+
         show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (move == 1 && player1cardsvisibility == false) {
+                if (move == 1 && !player1cardsvisibility) {
                     player1cards.setVisibility(View.VISIBLE);
                     player1cardsvisibility = true;
                     show.setText("Hide");
-                } else if (move == 1 && player1cardsvisibility == true) {
+                } else if (move == 1 && player1cardsvisibility) {
                     player1cards.setVisibility(View.INVISIBLE);
                     player1cardsvisibility = false;
                     show.setText("Show");
-                } else if (move == 2 && player2cardsvisibility == true) {
+                } else if (move == 2 && player2cardsvisibility) {
                     player2cards.setVisibility(View.INVISIBLE);
                     player2cardsvisibility = false;
                     show.setText("Show");
-                } else if (move == 2 && player2cardsvisibility == false) {
+                } else if (move == 2 && !player2cardsvisibility) {
                     player2cards.setVisibility(View.VISIBLE);
                     player2cardsvisibility = true;
                     show.setText("Hide");
@@ -68,15 +71,12 @@ public class game2playerActivity extends AppCompatActivity {
             }
         });
 
+
         step_change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (player1cardsvisibility == false && player2cardsvisibility == false) {
-                    if (move == 1) {
-                        move = 2;
-                    } else if (move == 2) {
-                        move = 1;
-                    }
+                if (!player1cardsvisibility && !player2cardsvisibility) {
+                    move = (move == 1) ? 2 : 1;
                 } else {
                     Toast.makeText(game2playerActivity.this, "Hide your cards", Toast.LENGTH_SHORT).show();
                 }
@@ -86,26 +86,26 @@ public class game2playerActivity extends AppCompatActivity {
         deal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int randomindexp1_1 = (int)(Math.random() * 18);
-                int randomindexp1_2 = (int)(Math.random() * 17);
-                int randomindexp1_3 = (int)(Math.random() * 16);
-                int randomindexp1_4 = (int)(Math.random() * 15);
-                int randomindexp1_5 = (int)(Math.random() * 14);
-                int randomindexp1_6 = (int)(Math.random() * 13);
-                int randomindexp1_7 = (int)(Math.random() * 12);
-                int randomindexp1_8 = (int)(Math.random() * 11);
-                int randomindexp1_9 = (int)(Math.random() * 10);
-                int randomindexp2_1 = (int)(Math.random() * 9);
-                int randomindexp2_2 = (int)(Math.random() * 8);
-                int randomindexp2_3 = (int)(Math.random() * 7);
-                int randomindexp2_4 = (int)(Math.random() * 6);
-                int randomindexp2_5 = (int)(Math.random() * 5);
-                int randomindexp2_6 = (int)(Math.random() * 4);
-                int randomindexp2_7 = (int)(Math.random() * 3);
-                int randomindexp2_8 = (int)(Math.random() * 2);
+                int randomindexp1_1 = (int) (Math.random() * 18);
+                int randomindexp1_2 = (int) (Math.random() * 17);
+                int randomindexp1_3 = (int) (Math.random() * 16);
+                int randomindexp1_4 = (int) (Math.random() * 15);
+                int randomindexp1_5 = (int) (Math.random() * 14);
+                int randomindexp1_6 = (int) (Math.random() * 13);
+                int randomindexp1_7 = (int) (Math.random() * 12);
+                int randomindexp1_8 = (int) (Math.random() * 11);
+                int randomindexp1_9 = (int) (Math.random() * 10);
+                int randomindexp2_1 = (int) (Math.random() * 9);
+                int randomindexp2_2 = (int) (Math.random() * 8);
+                int randomindexp2_3 = (int) (Math.random() * 7);
+                int randomindexp2_4 = (int) (Math.random() * 6);
+                int randomindexp2_5 = (int) (Math.random() * 5);
+                int randomindexp2_6 = (int) (Math.random() * 4);
+                int randomindexp2_7 = (int) (Math.random() * 3);
+                int randomindexp2_8 = (int) (Math.random() * 2);
                 int randomindexp2_9 = 0;
-                ArrayList<String> cards = new ArrayList<>(Arrays.asList("J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A", "*", "*"));
 
+                ArrayList<String> cards = new ArrayList<>(Arrays.asList("J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A", "*", "*"));
 
                 player1card1.setText(cards.get(randomindexp1_1));
                 cards.remove(randomindexp1_1);
@@ -143,7 +143,10 @@ public class game2playerActivity extends AppCompatActivity {
                 cards.remove(randomindexp2_8);
                 player2card9.setText(cards.get(randomindexp2_9));
                 cards.remove(randomindexp2_9);
+                deal.setEnabled(false);
+                deal.setVisibility(View.INVISIBLE);
 
+                Toast.makeText(game2playerActivity.this, "Cards have been dealt!", Toast.LENGTH_SHORT).show();
             }
         });
     }
